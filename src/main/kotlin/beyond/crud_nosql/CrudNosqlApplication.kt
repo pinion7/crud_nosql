11d9a2408d338232674c108bfe368fd33c5b7f3b
박민수 <pinion7rl@gmail.com>
2022-08-26 14:06:18 +0900

    Initial commit

---
+++ src/main/kotlin/beyond/crud_nosql/CrudNosqlApplication.kt
@@ -0,0 +1,11 @@
+package beyond.crud_nosql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudNosqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudNosqlApplication>(*args)
+}
---
+++ src/test/kotlin/beyond/crud_nosql/CrudNosqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package beyond.crud_nosql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudNosqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
